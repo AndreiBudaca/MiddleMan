861fe706f814036dc31263d90de1fa4eb41f0ca1
AndreiBudaca <marius-andrei.budaca@student.tuiasi.ro>
2024-09-08 13:07:56 +0300

    Add logout endpoint

--- MiddleMan.Web/Controllers/AuthenticationController.cs
+++ MiddleMan.Web/Controllers/AuthenticationController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MiddleMan.Web.Controllers
@@ -6,6 +8,7 @@ namespace MiddleMan.Web.Controllers
   [Route("[controller]")]
   public class AuthenticationController : Controller
   {
+    [HttpGet]
     [AllowAnonymous]
     [Route("Login")]
     public IActionResult Login()
@@ -14,6 +17,14 @@ namespace MiddleMan.Web.Controllers
         return RedirectToAction("Index", "Home"); 
 
       return Challenge();
+    }
+
+    [HttpGet]
+    [Route("Logout")]
+    public async Task<IActionResult> Logout()
+    {
+      await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+      return RedirectToAction("Index", "Home");
     }
   }
 }
